9b32db9e18bcc87de6d75c31f7f92807671d7a36
Paul Sohn <pksohn@gmail.com>
2020-12-10 12:36:04 -0800

    Day 1 part 2

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,19 +1,43 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day1 {
     public static void main(String[] args) {
+        // Part 1
         Scanner scanner = new Scanner(System.in);
-        HashSet<Integer> set = new HashSet<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        HashSet<Integer> set = new HashSet<>();
         while (scanner.hasNext()) {
             String currentLine = scanner.nextLine();
             int number = Integer.parseInt(currentLine);
             int complement = 2020 - number;
             if (set.contains(complement)) {
-                System.out.println("Solution: " + number * complement);
-                break;
+                System.out.println("Part 1 Solution: " + number * complement);
+                // Don't return early to initialize entire ArrayList for part 2.
             } else {
                 set.add(number);
+            }
+            numbers.add(number);
+        }
+
+        // Part 2
+        // Brute force.
+        int i;
+        for (i = 0; i < numbers.size(); i++) {
+            HashSet<Integer> tempSet = new HashSet<>();
+            int first = numbers.get(i);
+            int j;
+            for (j = i + 1; j < numbers.size(); j++) {
+                int second = numbers.get(j);
+                int complement = 2020 - first - second;
+                if (tempSet.contains(complement)) {
+                    System.out.println("Part 2 Solution: " + first * second * complement);
+                    break;
+                } else {
+                    tempSet.add(second);
+                }
             }
         }
     }
